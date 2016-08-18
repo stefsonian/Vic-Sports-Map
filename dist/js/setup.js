fb1f0993c2c9f40148aa51dc67f7928a653691c1
Stefan Stefansen <mail@stefanstefansen.com>
2016-08-18 16:12:50 +0200

    Place async google map load in setup.js

--- dist/js/setup.js
+++ dist/js/setup.js
@@ -1,6 +1,26 @@
+// Show the menu when the screen is wide enough
 if ($(window).width() > 300) {
     $('.toggle-button').click();
 }
+
+// ###Load Google Maps###
+// Load the menu scripts once the map is loaded (they depend on the map having been initialised)
+$(function () {
+  $.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCqQCJP3bsnODpjLx0dPilCEvstm20w7uY&v=3&callback=initMap", function () {
+
+    $.getScript("dist/js/mappy.js").fail(function(jqxhr, settings, exception) {
+      console.log('Failed to load script: mappy.js');
+    });
+
+    $.getScript("dist/js/ko.js").fail(function(jqxhr, settings, exception) {
+      console.log('Failed to load script: ko.js');
+    });
+
+  }).fail(function(jqxhr, settings, exception) {
+    console.log('Failed to load Google Map');
+  });
+
+});
 
 // ###Helper functions###
 // Convert to title case
